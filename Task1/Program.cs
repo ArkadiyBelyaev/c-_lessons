310169e1e5444fbe3e52f6f28a5d041aa3a9c60e
Arkady <arladibelyaev@gmail.com>
2024-02-06 16:37:29 +0300

    add some c# instructions

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello World!");
+﻿// Console.WriteLine("Hello World!");
+
+int value = 100;
+Console.WriteLine(value);
+
+int someNumber = 57;
+Console.WriteLine(someNumber);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);

1e52fe86611570218a8c1d6852899272dde6aa60
Arkady <arladibelyaev@gmail.com>
2024-02-07 19:54:48 +0300

    add new practic foilder

---
+++ lesson_practic/Program.cs
@@ -0,0 +1,35 @@
+﻿// Console.WriteLine("Введите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 == num2 * num2)
+// {
+// 	Console.WriteLine("Yes");
+// }
+// else
+// {
+// 	Console.WriteLine("No");
+// }
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int negNum = num * -1;
+
+// while (num >= negNum)
+// {
+// 	Console.Write(negNum);
+// 	negNum++;
+// }
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num / 100;
+int num2 = num % 10;
+
+int result = num1 + num2;
+// Console.WriteLine("Сумма = " + result + " рублей");
+Console.WriteLine($"Сумма = {result} рублей");

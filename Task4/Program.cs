f44cbf4367827820d41e377d4afd184f3007b5f9
Arkady <arladibelyaev@gmail.com>
2024-02-08 21:54:54 +0300

    some change

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,9 +1,16 @@
-﻿int n = 100;
-int i = 1;
+﻿// int n = 100;
+// int i = 1;
 
-while (i <= n)
-{
-	Console.Write(i);
-	Console.Write(' ');
-	i++;
-}
+// while (i <= n)
+// {
+// 	Console.Write(i);
+// 	Console.Write(' ');
+// 	i++;
+// }
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num / 100;
+int num3 = num % 10;
+
+int result = num1 * 10 + num3;
+Console.WriteLine(result);
